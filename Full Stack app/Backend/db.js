e19f2900a78d747d1aa52d866609a2c17f45af26
Samsursekh <sekhsamsur44@gmail.com>
2023-04-06 23:25:16 +0530

    added

--- Full Stack app/Backend/db.js
+++ Full Stack app/Backend/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
 
 const conncetFunction = async() => {
     try {
-        const connection = await mongoose.connect('mongodb+srv://samsursekh:54321@cluster0.64g4nkm.mongodb.net/?retryWrites=true&w=majority');
+        const connection = await mongoose.connect(`process.dotEnv.MONGO_URL`);
         console.log("Connected Database....");
+        connection.disconnect();
+
     } catch (error) {
         console.log(error);
+        console.log("Error connecting Happend...")
     }
 }
 
